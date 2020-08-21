da96c1c9b2f6d27adc788019258d9335611ce51e
fyodorschnotz <fyodorschnotz@gmail.com>
2020-08-21 15:29:06 +0700

    Menambahakn Ordering dengan main 1-3

---
+++ Numpy Tutorial/09 - Ordering/Main2.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a = np.floor(np.random.randn(2,2)*10) #syntax untuk ngeluarin array random
+
+print(a)
+
+print('nilai max dari a=', a.max())
+print('posisi max dari a =', a.argmax()) #ngambil posisi max
+print('nilai min dari a=', a.min())
+print('posisi min dari a =', a.argmin()) #ngambil posisi max
+
+#mengurutkan nilai dari a
+print('mengurutkan nilai a:')
+print(np.sort(a))
+print(np.argsort(a))
